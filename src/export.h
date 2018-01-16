8b5b0eee4d6d1db28bdaa2cbe953e39f9ed90ac2
Vadim Markovtsev <vadim@sourced.tech>
2018-01-16 00:36:31 -0800

    Use dllimport for external users on Windows

--- src/export.h
+++ src/export.h
@@ -1,6 +1,10 @@
 #ifndef EXPORT
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+#ifdef LIBUAST_BUILD
 #define EXPORT __declspec(dllexport)
+#else
+#define EXPORT __declspec(dllimport)
+#endif
 #else
 #define EXPORT
 #endif
