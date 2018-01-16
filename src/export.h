f8958c66e292e5df95b39ea5acb88a9f7c033f14
Vadim Markovtsev <vadim@sourced.tech>
2018-01-16 02:50:24 -0800

    Allow libuast to be statically linked on Windows

--- src/export.h
+++ src/export.h
@@ -1,5 +1,5 @@
 #ifndef EXPORT
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+#if !defined(LIBUAST_STATIC) && (defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__))
 #ifdef LIBUAST_BUILD
 #define EXPORT __declspec(dllexport)
 #else
